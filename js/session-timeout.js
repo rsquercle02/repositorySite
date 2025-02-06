da3e8727b87cd2cf0cad6dd8fce1f85a414ed1c7
RalphChristianGAbina <christian.abina2@gmail.com>
2025-02-06 17:15:45 +0800

    Update on the timeout variable.

--- js/session-timeout.js
+++ js/session-timeout.js
@@ -1,6 +1,6 @@
 
   // Set the session timeout to 30 minutes
-  var timeout = 1 * 60 * 1000; // 30 minutes in milliseconds
+  var timeout = 30 * 60 * 1000; // 30 minutes in milliseconds
 
   // Set a timer to check for inactivity
   setInterval(function() {
