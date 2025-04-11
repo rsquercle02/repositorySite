6475d35617e3cc151bb14192e146cb193f8fe6fe
RalphChristianGAbina <christian.abina2@gmail.com>
2025-04-11 09:22:25 +0800

    Update to file.

--- logout.php
+++ logout.php
@@ -1,17 +1,12 @@
 <?php
 // Start the session
-session_start();
+//session_start();
 
 // Destroy the session
 session_unset();
 session_destroy();
 
-// Set headers to prevent caching
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");  // Prevent cache
-header("Pragma: no-cache");  // Prevent cache
-header("Expires: 0");  // Set expiration date to past
-
 // Redirect to login page with a 301 status code (permanent redirect)
-header("Location: login.php", true, 301);
+header("Location: index.php");
 exit;
 ?>
