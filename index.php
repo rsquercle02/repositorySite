123c142a481a0785f15e5b809fd47309ec932f18
RalphChristianGAbina <christian.abina2@gmail.com>
2025-04-11 10:38:34 +0800

    Update to file.

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if (isset($_SESSION['id'])) {
     <!-- Fonts CSS -->
     <link href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="loginstyle.css">
+    <link rel="stylesheet" href="css/loginstyle.css">
     <title>Login</title>
 </head>
 
